84b816f16ca9240eb64709bcfbefe1fd1174e6b1
Bogdan STefan Niculică <bogdan.niculica@student.usv.ro>
2021-10-28 00:32:17 +0300

    temporary

---
+++ Niculica_Bogdan_3132b_Lab3/Elementar.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace Niculica_Bogdan_3132b_Lab3
+{
+    class Elementar : GameWindow
+    {
+
+        private const int XYZ_SIZE = 75;
+
+        public Elementar() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+            Console.WriteLine("OpenGl versiunea: " + GL.GetString(StringName.Version));
+            Title = "OpenGl versiunea: " + GL.GetString(StringName.Version) + " (mod imediat)";
+
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.Blue);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, -100, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+
+            Matrix4 lookat = Matrix4.LookAt(30, 30, 30, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+
+            if (keyboard[Key.Escape])
+            {
+                Exit();
+            }
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+
+
+
+            DrawAxes();
+
+            DrawObjects();
+
+
+
+
+            SwapBuffers();
+        }
+
+        private void DrawAxes()
+        {
+
+
+
+
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(XYZ_SIZE, 0, 0);
+
+            System.Threading.Thread.Sleep(50);
+
+
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, XYZ_SIZE, 0); ;
+
+            System.Threading.Thread.Sleep(50);
+
+
+            GL.Color3(Color.Green);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, XYZ_SIZE);
+
+            System.Threading.Thread.Sleep(50);
+            GL.End();
+        }
+
+        private void DrawObjects()
+        {
+
+
+
+        }
+
+
+        
+    }
+
+}
---
+++ Niculica_Bogdan_3132b_Lab3/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+using System.IO;
+//Problema 8 + 9
+namespace Niculica_Bogdan_3132b_Lab3
+{
+    class TriunghiColor : GameWindow
+    {
+        float R1,R2,R3, G1,G2,G3, B1,B2,B3;
+        bool pressedFirst, pressedSecond, pressedThird;
+
+        public TriunghiColor() : base(800, 600,OpenTK.Graphics.GraphicsMode.Default,"Schimba culoare triunghi")
+        {
+            KeyDown += Keyboard_KeyDown;
+            R1 = R2 = R3 = 0.9f;
+            G1 = G2 = G3 = 0.8f;
+            B1 = B2 = B3 = 0.7f;
+            pressedFirst = pressedSecond = pressedThird = false;
+            
+            
+        }
+
+
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.Gray);
+            
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
+            //Rezolvare problema 8+9; memorarea culorii fiecarui vertex separat in 9 variabile nu este frumoasa! Revin cu modificari
+            //Scad culoare
+            if (keyboard[Key.Left] && keyboard[Key.R])
+            {
+                if(pressedFirst)
+                    R1 = R1 - 0.005f;
+                else if(pressedSecond)
+                    R2 = R2 - 0.005f;
+                else if(pressedThird)
+                    R3 = R3 - 0.005f;
+                else
+                {
+                    R1 = R1 - 0.005f;
+                    R2 = R2 - 0.005f;
+                    R3 = R3 - 0.005f;
+                }
+                Log($"Decrease - R1={R1};R2={R2};R3={R3}");
+            }
+            if (keyboard[Key.Left] && keyboard[Key.G])
+            {
+                if (pressedFirst)
+                    G1 = G1 - 0.005f;
+                else if (pressedSecond)
+                    G2 = G2 - 0.005f;
+                else if (pressedThird)
+                    G3 = G3 - 0.005f;
+                else
+                {
+                    G1 = G1 - 0.005f;
+                    G2 = G2 - 0.005f;
+                    G3 = G3 - 0.005f;
+                }
+                Log($"Decrease - G1={G1};G2={G2};G3={G3}");
+            }
+            if (keyboard[Key.Left] && keyboard[Key.B])
+            {
+                if (pressedFirst)
+                    B1 = B1 - 0.005f;
+                else if (pressedSecond)
+                    B2 = B2 - 0.005f;
+                else if (pressedThird)
+                    B3 = B3 - 0.005f;
+                else
+                {
+                    B1 = B1 - 0.005f;
+                    B2 = B2 - 0.005f;
+                    B3 = B3 - 0.005f;
+                }
+                Log($"Decrease - B1={B1};B2={B2};B3={B3}");
+            }
+            //cresc culoare
+            if (keyboard[Key.Right] && keyboard[Key.R])
+            {
+                if (pressedFirst)
+                    R1 = R1 + 0.005f;
+                else if (pressedSecond)
+                    R2 = R2 + 0.005f;
+                else if (pressedThird)
+                    R3 = R3 + 0.005f;
+                else
+                {
+                    R1 = R1 + 0.005f;
+                    R2 = R2 + 0.005f;
+                    R3 = R3 + 0.005f;
+                }
+                Log($"Increase - R1={R1};R2={R2};R3={R3}");
+            }
+            if (keyboard[Key.Right] && keyboard[Key.G])
+            {
+                if (pressedFirst)
+                    G1 = G1 + 0.005f;
+                else if (pressedSecond)
+                    G2 = G2 + 0.005f;
+                else if (pressedThird)
+                    G3 = G3 + 0.005f;
+                else
+                {
+                    G1 = G1 + 0.005f;
+                    G2 = G2 + 0.005f;
+                    G3 = G3 + 0.005f;
+                }
+                Log($"Increase - G1={G1};G2={G2};G3={G3}");
+            }
+            if (keyboard[Key.Right] && keyboard[Key.B])
+            {
+                if (pressedFirst)
+                    B1 = B1 + 0.005f;
+                else if (pressedSecond)
+                    B2 = B2 + 0.005f;
+                else if (pressedThird)
+                    B3 = B3 + 0.005f;
+                else
+                {
+                    B1 = B1 + 0.005f;
+                    B2 = B2 + 0.005f;
+                    B3 = B3 + 0.005f;
+                }
+                Log($"Increase - B1={B1};B2={B2};B3={B3}");
+            }
+
+
+            
+            if (keyboard[Key.Number1])
+            {
+                Log("vertex 1!");
+                pressedFirst = true;
+                pressedSecond = pressedThird = false;
+
+            }
+            if (keyboard[Key.Number2])
+            {
+                Log("vertex 2!");
+                pressedSecond = true;
+                pressedFirst = pressedThird = false;
+            }
+            if (keyboard[Key.Number3])
+            {
+                Log("vertex 3!");
+                pressedThird = true;
+                pressedFirst = pressedSecond = false;
+            }
+
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+            GL.Begin(PrimitiveType.Triangles);
+            
+            GL.Color3(R1, G1, B1);
+            GL.Vertex2(0.0f, 1.0f);
+            GL.Color3(R2, G2, B2);
+            GL.Vertex2(-1.0f, -1.0f);
+            GL.Color3(R3, G3, B3);
+            GL.Vertex2(1.0f, -1.0f);
+
+            GL.End();
+
+
+            this.SwapBuffers();
+        }
+        void Log(string msg, string file="log.txt")
+        {
+            Console.WriteLine(msg);
+            using (StreamWriter writetext = new StreamWriter(file))
+            {
+                writetext.WriteLine(msg);
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+
+            using (TriunghiColor example = new TriunghiColor())
+            {
+
+
+                example.Run(30.0, 0.0);
+
+            }
+        }
+
+
+    }
+
+
+
+
+}
+
